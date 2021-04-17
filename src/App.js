0c0441d47dd7e22d382e33c43f906170753e534b
Sara Taylor <sara.swifti@gmail.com>
2021-04-17 15:07:45 -0400

    cleaning up react folders that were imported

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-//import logo from './logo.svg';
-//import './App.css';
+;
+
 import React from "react";
 import Jumbotron from "./components/Jumbotron";
 import Card from "./components/Card";
@@ -7,13 +7,12 @@ import NavBar from "./components/NavBar";
 
 
 function App() {
-  return (
-    <div classNAme="container">
-      <NavBar />;
-      <Jumbotron />;
-      <Card />;
-    </div>
-  );
+  return  (null;
+    // <div classNAme="container">
+    //   <NavBar />;
+    //   <Jumbotron />;
+    //   <Card />;
+    // </div>
 }
 
 export default App;
