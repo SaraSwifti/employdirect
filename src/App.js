969515f048c8d0703b9f7f652169ad76db9e3817
Sara Taylor <sara.swifti@gmail.com>
2021-04-17 22:30:57 -0400

    break throughm withm booststrap components rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-;
 
+//importing components
 import React from "react";
-import Jumbotron from "./components/Jumbotron";
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
 
 
 function App() {
-  return  (null;
-    // <div classNAme="container">
-    //   <NavBar />;
-    //   <Jumbotron />;
-    //   <Card />;
-    // </div>
+  return (
+    <>
+      <NavBar />;
+     
+      <Card />;
+    </>
+  )
 }
 
 export default App;
